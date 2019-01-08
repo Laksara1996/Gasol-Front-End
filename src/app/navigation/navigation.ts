df4ed46640a41986813968109b03c6e6607731a1
Gayal Laksara <gayallaksara1996@gmail.com>
2019-01-08 21:33:00 +0530

    Second Commit

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -2,199 +2,65 @@ import { FuseNavigation } from '@fuse/types';
 
 export const navigation: FuseNavigation[] = [
     {
-        id       : 'applications',
-        title    : 'Applications',
-        translate: 'NAV.APPLICATIONS',
-        type     : 'group',
-        icon     : 'apps',
-        children : [
-            {
-                id       : 'dashboards',
-                title    : 'Dashboards',
-                translate: 'NAV.DASHBOARDS',
-                type     : 'collapsable',
-                icon     : 'dashboard',
-                children : [
-                    {
-                        id   : 'analytics',
-                        title: 'Analytics',
-                        type : 'item',
-                        url  : '/apps/dashboards/analytics'
-                    },
-                    {
-                        id   : 'project',
-                        title: 'Project',
-                        type : 'item',
-                        url  : '/apps/dashboards/project'
-                    }
-                ]
-            },
-            {
-                id       : 'calendar',
-                title    : 'Calendar',
-                translate: 'NAV.CALENDAR',
-                type     : 'item',
-                icon     : 'today',
-                url      : '/apps/calendar'
-            }
-            
-        ]
+        id   : 'analytics',
+        title: 'Analytics',
+        type : 'item',
+        url  : '/apps/dashboards/analytics'
     },
+
     {
-        id      : 'pages',
-        title   : 'Pages',
-        type    : 'group',
-        icon    : 'pages',
-        children: [
-            {
-                id      : 'authentication',
-                title   : 'Authentication',
-                type    : 'collapsable',
-                icon    : 'lock',
-                badge   : {
-                    title: '10',
-                    bg   : '#525e8a',
-                    fg   : '#FFFFFF'
-                },
-                children: [
-                    {
-                        id   : 'login',
-                        title: 'Login',
-                        type : 'item',
-                        url  : '/pages/auth/login'
-                    },
-                    {
-                        id   : 'login-v2',
-                        title: 'Login v2',
-                        type : 'item',
-                        url  : '/pages/auth/login-2'
-                    },
-                    {
-                        id   : 'register',
-                        title: 'Register',
-                        type : 'item',
-                        url  : '/pages/auth/register'
-                    },
-                    {
-                        id   : 'register-v2',
-                        title: 'Register v2',
-                        type : 'item',
-                        url  : '/pages/auth/register-2'
-                    },
-                    {
-                        id   : 'forgot-password',
-                        title: 'Forgot Password',
-                        type : 'item',
-                        url  : '/pages/auth/forgot-password'
-                    },
-                    {
-                        id   : 'forgot-password-v2',
-                        title: 'Forgot Password v2',
-                        type : 'item',
-                        url  : '/pages/auth/forgot-password-2'
-                    },
-                    {
-                        id   : 'reset-password',
-                        title: 'Reset Password',
-                        type : 'item',
-                        url  : '/pages/auth/reset-password'
-                    },
-                    {
-                        id   : 'reset-password-v2',
-                        title: 'Reset Password v2',
-                        type : 'item',
-                        url  : '/pages/auth/reset-password-2'
-                    },
-                    {
-                        id   : 'lock-screen',
-                        title: 'Lock Screen',
-                        type : 'item',
-                        url  : '/pages/auth/lock'
-                    },
-                    {
-                        id   : 'mail-confirmation',
-                        title: 'Mail Confirmation',
-                        type : 'item',
-                        url  : '/pages/auth/mail-confirm'
-                    }
-                ]
-            },
-            {
-                id   : 'coming-soon',
-                title: 'Coming Soon',
-                type : 'item',
-                icon : 'alarm',
-                url  : '/pages/coming-soon'
-            },
-            {
-                id      : 'errors',
-                title   : 'Errors',
-                type    : 'collapsable',
-                icon    : 'error',
-                children: [
-                    {
-                        id   : '404',
-                        title: '404',
-                        type : 'item',
-                        url  : '/pages/errors/error-404'
-                    },
-                    {
-                        id   : '500',
-                        title: '500',
-                        type : 'item',
-                        url  : '/pages/errors/error-500'
-                    }
-                ]
-            },
-            
-            {
-                id      : 'pricing',
-                title   : 'Pricing',
-                type    : 'collapsable',
-                icon    : 'attach_money',
-                children: [
-                    {
-                        id   : 'style-1',
-                        title: 'Style 1',
-                        type : 'item',
-                        url  : '/pages/pricing/style-1'
-                    },
-                    {
-                        id   : 'style-2',
-                        title: 'Style 2',
-                        type : 'item',
-                        url  : '/pages/pricing/style-2'
-                    },
-                    {
-                        id   : 'style-3',
-                        title: 'Style 3',
-                        type : 'item',
-                        url  : '/pages/pricing/style-3'
-                    }
-                ]
-            },
-            {
-                id   : 'profile',
-                title: 'Profile',
-                type : 'item',
-                icon : 'person',
-                url  : '/pages/profile'
-            },
-            {
-                id   : 'search',
-                title: 'Search',
-                type : 'item',
-                icon : 'search',
-                url  : '/pages/search'
-            },
-            {
-                id   : 'faq',
-                title: 'Faq',
-                type : 'item',
-                icon : 'help',
-                url  : '/pages/faq'
-            }
-        ]
+        id   : 'project',
+        title: 'Project',
+        type : 'item',
+        url  : '/apps/dashboards/project'
+    },
+
+    {
+        id   : 'form',
+        title: 'Form',
+        type : 'item',
+        url  : '/apps/dashboards/form'
+    },
+
+    {
+        id       : 'calendar',
+        title    : 'Calendar',
+        translate: 'NAV.CALENDAR',
+        type     : 'item',
+        icon     : 'today',
+        url      : '/apps/calendar'
+    }
+    
+    {
+        id   : 'coming-soon',
+        title: 'Coming Soon',
+        type : 'item',
+        icon : 'alarm',
+        url  : '/pages/coming-soon'
+    },
+    
+    {
+        id   : 'faq',
+        title: 'Faq',
+        type : 'item',
+        icon : 'help',
+        url  : '/pages/faq'
+    },
+
+    {
+        id   : 'knowledge-base',
+        title: 'Knowledge-base',
+        type : 'item',
+        icon : 'import_contacts',
+        url  : '/pages/knowledge-base'
+    },
+
+    {
+        id   : 'change-log',
+        title: 'Change-log',
+        type : 'item',
+        icon : 'update',
+        url  : '/documentation/change-log'
     }
 
 ];
